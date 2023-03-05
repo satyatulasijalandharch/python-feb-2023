0c622018f673d14c91faa4f1876da1804d8cea4a
Lokesh Soni <ynkshatriya@gmail.com>
2023-03-05 08:40:17 +0530

    update class work of 3 & 4 mar 2023

--- class work/bot_run.py
+++
@@ -1,8 +0,0 @@
-import faq
-import user
-
-
-question = user.get_user_input()
-reply = faq.faq_bot(question)
-
-print(reply)
--- class work/knowledge_bank.py
+++
@@ -1,6 +0,0 @@
-KNOWLEDGE_BANK = {
-    "Who is the PM of India?": "Narendra Modi",
-    "When did India get its independence?": "1947",
-    "What is the currency of India?": "Rupee",
-    "What is the currency of US?": "Dollar"
-}
