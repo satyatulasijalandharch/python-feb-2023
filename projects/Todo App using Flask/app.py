be5b959faa7cf2cd0a700ae77d5d299c511c4c4e
Lokesh Soni <ynkshatriya@gmail.com>
2023-03-20 11:48:46 +0530

    20 mar update 1

--- projects/Todo App using Flask/app.py
+++ projects/Todo App using Flask/app.py
@@ -1,7 +1,7 @@
-# pip install flask
+from uuid import uuid4
+from datetime import datetime
 
-from flask import Flask
-# app = Flask("ToDO")
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
@@ -16,7 +16,7 @@ TASKS = {
 
     'a432b999-5f5d-4662-bacd-dee8edbda7d7': {
         'task_id': 'a432b999-5f5d-4662-bacd-dee8edbda7d7',
-        'task': 'Talk to karthik',
+        'task': 'second task',
         'created_at': '2023-03-16T08:51:38.673934',
         'last_updated_at': '2023-03-16T08:51:38.673934',
         'status': 'In Progress'
@@ -26,23 +26,110 @@ TASKS = {
 
 @app.route("/tasks", methods=["GET"])
 def get_all_tasks():
-    keys = ["task_id", "task", "status"]
+    if TASKS:
+        keys = ["task_id", "task", "status"]
 
-    all_tasks = []
+        all_tasks = []
 
-    for task_details in TASKS.values():
-        task_details_to_return = {}
-        for key, value in task_details.items():
-            if key in keys:
-                task_details_to_return[key] = value
+        for task_details in TASKS.values():
+            task_details_to_return = {}
+            for key, value in task_details.items():
+                if key in keys:
+                    task_details_to_return[key] = value
 
-        all_tasks.append(task_details_to_return)
+            all_tasks.append(task_details_to_return)
 
-    return all_tasks
+        response = {
+            "tasks": all_tasks,
+            "count": len(all_tasks)
+        }
+
+        return jsonify(response), 200
+
+    else:
+        return jsonify({"error": "No Tasks Found"}), 400
+
+
+@app.route("/tasks", methods=["POST"])
+def create_task():
+    request_data = request.json
+
+    if "task" in request_data:
+        task_statement = request_data["task"]
+
+        current_timestamp = datetime.now().isoformat()
+        task_id = str(uuid4())
+
+        task = {
+            "task_id": task_id,
+            "task": task_statement,
+            "created_at": current_timestamp,
+            "last_updated_at": current_timestamp,
+            "status": "In Progress"
+        }
+
+        TASKS[task_id] = task
+        return jsonify(task), 201
+
+    else:
+        return jsonify({"error": "Invalid request body"}), 400
+
+
+@app.route("/tasks/<taskId>", methods=["GET"])
+def get_a_task(taskId):
+    if taskId in TASKS:
+        task = TASKS[taskId]
+        return jsonify(task), 200
+    else:
+        return jsonify({"error": "Task id is invalid"}), 400
+
+
+@app.route("/tasks/<taskId>", methods=["PATCH"])
+def update_a_task(taskId):
+    request_data = request.json
+    print(request_data)
+
+    if "task" in request_data and taskId in TASKS:
+        task_statement = request_data["task"]
+
+        task = TASKS[taskId]
+        status = task["status"]
+
+        if status == "Completed":
+            return "Completed task cannot be updated", 400
+
+        task["task"] = task_statement
+        task["last_updated_at"] = datetime.now().isoformat()
+
+        return jsonify(task), 200
+    else:
+        return jsonify({"error": "Task id is invalid"}), 400
+
+
+@app.route("/tasks/<taskId>/complete", methods=["POST"])
+def complete_a_task(taskId):
+    if taskId in TASKS:
+        task = TASKS[taskId]
+
+        task["status"] = "Completed"
+        task["last_updated_at"] = datetime.now().isoformat()
+
+        return jsonify(task), 200
+    else:
+        return jsonify({"error": "Task id is invalid"}), 400
+
+
+@app.route("/tasks/<taskId>", methods=["DELETE"])
+def delete_a_task(taskId):
+    if taskId in TASKS:
+        _ = TASKS.pop(taskId)
+        return jsonify({"message": "Task deleted"}), 200
+    else:
+        return jsonify({"error": "Task id is invalid"}), 400
 
 
 app.run(
     debug=True,
-    port=80,
-    host="0.0.0.0"
+    # port=80,
+    # host="0.0.0.0"
 )
